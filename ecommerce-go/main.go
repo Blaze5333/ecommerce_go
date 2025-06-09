616657ad3e69b79d93199a1ab302d07af57648d0
arwachaiwala@gmail.com <mustafachaiwala2003@gmail.com>
2025-06-09 11:48:14 +0530

    project setup done

--- ecommerce-go/main.go
+++ ecommerce-go/main.go
@@ -26,4 +26,5 @@ func main() {
 	router.GET("/cartcheckout", app.BuyFromCart())
 	router.GET("/instantly", app.InstantBuy())
 	log.Fatal(router.Run(":" + port))
+	log.Println("Server running on port:", port)
 }
